f4ccbdd4ceecbdb03f770b3ee1dc05f026fa3130
Sanjay <thapasanjay107.st@gmail.com>
2023-04-25 20:12:33 +0545

    Final Commit

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.project;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button browser,save;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        browser = findViewById(R.id.btnopen);
+        save = findViewById(R.id.btnadd);
+
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(i);
+            }
+        });
+    }
+
+    public void SendMessage(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondPageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.name);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+}
